fc2a3acc8d425528fcbc1ec64ca73706ef65d1aa
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-24 12:12:34 -0400

    added connections

---
+++ server.js
@@ -0,0 +1,8 @@
+const mysql = require('mysel');
+const inquirer = require('inquirer');
+//const consoleTable = require('console.table');
+
+var managers = [];
+var roles = [];
+var employees = [];
+
