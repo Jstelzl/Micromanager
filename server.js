18a731c4c3f60bcfe6953ef1ad65a4a718eeb8fb
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-24 12:25:33 -0400

    worked on server, created get manager function

--- server.js
+++ server.js
@@ -6,3 +6,36 @@ var managers = [];
 var roles = [];
 var employees = [];
 
+const getManager = () => {
+    connection.query(`SELECT manager, manager_id FROM managers`, (err, res) => {
+        if (err) throw err;
+        managers = [];
+        for (let i = 0; i < res.lenth; i++) {
+            const manager = res[i].manager;
+            const manager_id = res[i].manager;
+            var newManager = {
+                name: manager,
+                value: manager_id
+            }
+            managers.push(newManager);
+        }
+        return managers;
+    });
+};
+
+const getRole = () => {
+
+};
+
+const getEmployee = () => {
+
+};
+
+const init = () => {
+    getEmployee();
+    getRole();
+    getManager();
+};
+
+
+init();
