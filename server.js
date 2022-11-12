7339939f90ff5e7c1f9cad60b1d8db9abee76f1b
[josiah stelzl] <josiahstelzl@gmail.com>
2022-11-12 10:49:58 -0500

    edited connections file

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ const viewAllEmployees = () => {
     connection.query(`SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.name AS department,
      role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id
      LEFT JOIN department ON role.department_id = department.id
-     LEFT JOIN employee manager ON manager.id = employee.manager_id;`)
+     LEFT JOIN employee manager ON manager.employee_id = employee.manager_id;`)
         //     , (err, res) => {
         //     console.log("\nAll EMPLOYEES\n");
         //     if (err) throw err;
